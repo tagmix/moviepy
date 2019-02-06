6fcc093115f923be955d4e1d4123543338c57cec
Danny Shisler <danny@shisler.com>
2019-02-06 14:09:17 +0000

    Numba vectorization

--- moviepy/video/tools/drawing.py
+++ moviepy/video/tools/drawing.py
@@ -6,14 +6,19 @@ import numpy as np
 from numba import vectorize, uint8, float64
 
 
-@vectorize([float64(float64, uint8, float64)], target='parallel')
+# Use this proxy method to call the vectorized method to allow meaningful profiling
+def blit_fast(im1, im2, mask):
+    return blit_fast_vectorized(im1, im2, mask)
+
+
+@vectorize([float64(float64, float64, float64)], target='cpu')
 def blit_fast_vectorized(im1, im2, mask):
-    return 1.0 * mask * im1 + (1.0 - mask) * im2
+    return (mask * im1) + ((1.0 - mask) * im2)
 
 
 def blit(im1, im2, pos=None, mask=None, ismask=False):
     """ Blit an image over another.
-    
+
     Blits ``im1`` on ``im2`` as position ``pos=(x,y)``, using the
     ``mask`` if provided. If ``im1`` and ``im2`` are mask pictures
     (2D float arrays) then ``ismask`` must be ``True``.
@@ -22,8 +27,7 @@ def blit(im1, im2, pos=None, mask=None, ismask=False):
     # DS Experimental speedup, assumes POS is [0,0] and im1 and im2 are the same shape
     if mask is not None and not ismask:
         mask = np.dstack(3 * [mask])
-        return blit_fast_vectorized(im1, im2, mask)
-        #   return 1.0 * mask * im1 + (1.0 - mask) * im2
+        return blit_fast(im1, im2.astype(float), mask)
     elif not ismask:
         return im1
 
